b684554a1765270fd6f565b925c0eb878fff1757
Ponlponl123 <my.devpattarapong@gmail.com>
2025-04-27 16:53:48 +0700

    translated

--- src/app/api/play/route.ts
+++ src/app/api/play/route.ts
@@ -7,7 +7,7 @@ const lookup = promisify(dns.lookup);
 export async function GET() {
   try {
     const address = await lookup("direct.ponlponl123.com");
-    return NextResponse.json({ ip: address.address, port: 28017 }, { status: 200 });
+    return NextResponse.json({ ip: address.address, port: 28015 }, { status: 200 });
   } catch (err: any) {
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
